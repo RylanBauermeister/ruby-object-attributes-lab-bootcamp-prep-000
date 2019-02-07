31e24ffc5ca6d4508f967e5f9ea04ec71c52122c
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-07 01:28:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,17 @@
 class Person
+    def name=(human_name)
+    @human_name = human_name
+  end
   
+  def name
+    @human_name
+  end
+  
+  def job=(def_job)
+    @job = def_job
+  end
+  
+  def job
+    @job
+  end
+end
